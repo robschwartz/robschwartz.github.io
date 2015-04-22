359fe643db173e1eb70f5efc55e6fd6adf899c9b
robschwartz <robertschwar@gmail.com>
2015-04-21 21:24:04 -0500

    added elevator function

--- js/init.js
+++ js/init.js
@@ -1,9 +1,6 @@
-/*
-	Read Only by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 (function($) {
+
 
 	skel.init({
 		reset: 'full',
